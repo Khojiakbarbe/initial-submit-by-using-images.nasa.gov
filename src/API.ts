89382cf63e32ee397bf5da47754533efc4d72c88
Khojiakbarbe <121534562+Khojiakbarbe@users.noreply.github.com>
2023-09-16 00:51:34 +0500

    API ts deleted

--- src/API.ts
+++
@@ -1,9 +0,0 @@
-const request = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&quotaUser=Red%20Bull%20Surfing&key=AIzaSyBGSBrc6d-M5-iku5HPzig18upPizogjlQ'
-export default request 
-
-// Authorization: Bearer [YOUR_ACCESS_TOKEN]
-// Accept: application/json
- 
-
-// Authorization: Bearer [YOUR_ACCESS_TOKEN]
-// Accept: application/json
